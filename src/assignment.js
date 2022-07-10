e56c88cf1fa297dae010f5dc6a05038112d4c59e
Yvonne Dien <108582504+YvonneDien@users.noreply.github.com>
2022-07-10 10:16:25 +0000

    i did challenge 1 and 2.

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,14 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+ function sumOfNumbers (arrayOfNumbers) {
+   let number = 0;
+   for(let i = 0; i < arrayOfNumbers.length; i++) {
+     number += arrayOfNumbers[i];
+   }
+   return number;
+ }
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let number = 0;
+   for(let i = 0; i < arrayOfNumbers.length; i++) {
+     if(arrayOfNumbers[i]%2==0) {number ++;
+  }
+   }
+ 
+//console.log (number);
+  return number;
+  //return arrayOfNumbers.length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
